4805efbadcb6be1561c9c38c817ad19978e08345
Shane Graham <shane.alex.graham@gmail.com>
2020-11-24 19:40:58 +0000

    initial commit

---
+++ src/models/notes.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const notesSchema = new mongoose.Schema({
+  name: String,
+  body: String,
+  group: { type: mongoose.Schema.Types.ObjectId, ref: 'NotesCollection' }
+});
+
+const Notes = mongoose.model('Notes', notesSchema);
+
+exports.Notes = Notes;
