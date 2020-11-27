97cddbfda743dcd4f2b1276d5cd09139c21bed70
Shane Graham <shane.alex.graham@gmail.com>
2020-11-27 19:27:03 +0000

    removed indexes

--- src/models/notes.model.js
+++ src/models/notes.model.js
@@ -1,31 +1,11 @@
 const mongoose = require('mongoose');
 
 const notesSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    text: true
-  },
-  body: {
-    type: String,
-    text: true
-  },
+  name: String,
+  body: String,
+  short: String,
   group: { type: mongoose.Schema.Types.ObjectId, ref: 'NotesCollection' }
 });
-
-notesSchema.index(
-    {
-        name: 'text',
-        body: 'text'
-    },
-    {
-        name: 'NotesTextIndex',
-        default_language: 'english',
-        weights: {
-            name: 5,
-            body: 10,
-        },
-    }
-);
 
 const Notes = mongoose.model('Notes', notesSchema);
 
